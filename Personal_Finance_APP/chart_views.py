f1e6e995ba82b41d39bd7cb2203492f2d689d4ab
DrewJenn <drewj32@icloud.com>
2025-02-26 11:30:30 -0600

    Update chart_views.py
    reworked account_details function to add a transaction type column to table and fixed an issue with incompatible data types

--- Personal_Finance_APP/chart_views.py
+++ Personal_Finance_APP/chart_views.py
@@ -11,20 +11,25 @@ def account_details(request, id):
     account = get_object_or_404(BankAccount, id=id)
     currency = account.get_currency()
     records = BankRecord.objects.filter(account_id=account).order_by('transaction_date')
-    x_start_point, y_start_point = account.initial_account_date, float(account.get_initial_amount()) 
-    x_axis, y_axis = [x_start_point], [y_start_point]       
+    dates, balances = [account.initial_account_date], [float(account.get_initial_amount())]       
     transaction = [Money(account.get_initial_amount(), currency)]
+    transaction_type = ["Deposit"]
     for record in records:    
         if record.transaction_date and record.new_balance:
             if record.withdrawal_record:
-                transaction.append(Money(-1 * record.uninitialized_amount, currency))
+                transaction.append(Money(record.uninitialized_amount, currency))
+                transaction_type.append("Withdrawal")
             else:
                 transaction.append(record.transaction_amount)
-            x_axis.append(record.transaction_date)
-            y_axis.append(float(record.new_balance))
-    table_html = pd.DataFrame({'Date':x_axis, 'Amount':transaction, 'Balance':y_axis})
+                transaction_type.append("Deposit")
+            dates.append(record.transaction_date)
+            balances.append(float(record.new_balance))
+    table_html = pd.DataFrame({'Date':dates, 'Amount':transaction, 'Balance':balances, 'Transaction Type':transaction_type})
+    table_html_1 = []
     for i in range(table_html.shape[0]):
-        table_html.at[i, 'Balance'] = Money(table_html.at[i, 'Balance'], currency)
+        table_html_1.append(Money(table_html.at[i, 'Balance'], currency))
+    table_html["Balance"] = table_html_1
+    print(table_html)
     table_html = table_html.to_html(index=False)
     return render(request, 'data_display/account_details.html', {'table_html':table_html})
 
